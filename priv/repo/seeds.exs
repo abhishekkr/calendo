59398c4e8fae13ad1adc34b3e0f675deca8b62f7
AbhishekKr <abhikumar163@gmail.com>
2022-07-09 17:44:26 +0530

    add Event & EventType schema, add EventType seed
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,38 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+#
+
+alias Calendo.{EventType, Repo}
+
+Repo.delete_all(EventType)
+
+event_types = [
+  %{
+    name: "15 minute meeting",
+    description: "Short meeting call.",
+    slug: "15-min-meeting",
+    duration: 15,
+    color: "blue"
+  },
+  %{
+    name: "30 minute meeting",
+    description: "Extended meeting call.",
+    slug: "30-min-meeting",
+    duration: 30,
+    color: "pink"
+  },
+  %{
+    name: "An hour long discussion",
+    description: "One hour of catch-up.",
+    slug: "catchup-60",
+    duration: 60,
+    color: "purple"
+  }
+]
+
+for event_type <- event_types do
+  event_type
+  |> EventType.changeset()
+  |> Repo.insert!()
+end
